84f0b2fc6cd7406650302ec25e67503b15f83962
ezequiel magi <ezequielmaggi@gmail.com>
2022-01-11 17:50:27 -0300

    creado el modal en mobil

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 const cHeader = document.querySelectorAll('.collapsible-header') 
 const cbody = document.querySelectorAll('.collapsible-body')
+const headerButton = document.getElementById('header-button')
+const headerButtonClose = document.getElementById('header-button-close')
+const modal = document.querySelector('.modal')
+const main = document.querySelector('main')
+const footer = document.querySelector('footer')
 
 
+headerButton.addEventListener('click', ()=>{
+  headerButton.classList.toggle('hide')
+  headerButtonClose.classList.toggle('hide')
+  modal.classList.toggle('hide')
+  main.classList.toggle('hide')
+  footer.classList.toggle('hide')
+})
+
+headerButtonClose.addEventListener('click', ()=>{
+   headerButton.classList.toggle('hide')
+  headerButtonClose.classList.toggle('hide')
+  modal.classList.toggle('hide')
+  main.classList.toggle('hide')
+  footer.classList.toggle('hide')
+})
 
 
 cHeader.forEach( itemHeader => {
